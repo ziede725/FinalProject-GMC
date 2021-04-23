9e82185d5cfa8e69c209d182d27f0020c0cc4f4c
ziede <ziede.ben.yahya@gmail.com>
2021-04-23 20:48:39 +0100

    Theater controller

--- models/Theater.js
+++ models/Theater.js
@@ -36,6 +36,42 @@ const theaterSchema = new mongoose.Schema({
   screenings: [{ type: Schema.Types.ObjectId, ref: "Screening" }],
 });
 
-//Methods bcrypt hash password, compare password ..
 
-module.exports = mongoose.model("Theater", theaterSchema);
+
+//Methods bcrypt hash password, compare password ..
+theaterSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//Match Passwords
+theaterSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//Get Signed Token
+theaterSchema.methods.getSignedToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+//Generate
+
+theaterSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
+  return resetToken;
+};
+
+const Theater = mongoose.model("Theater", theaterSchema) ;  
+module.exports= Theater; 
