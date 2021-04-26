8c67a0a06a87b042236b4c241b0a8ddcc375911a
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2021-04-26 17:59:17 +0100

    genre functionnality updated

--- routes/genre.route.js
+++ routes/genre.route.js
@@ -1,11 +1,19 @@
-const router = require('express').Router() ; 
-
-// Routes for Genre 
+const router = require("express").Router();
+const genreController = require("../controllers/genreController");
+// Routes for Genre
 //
 //Should be public for Admin/MovieTheater ???
 
-router.use('/',) ; 
+router.post("/", genreController.createGenre);
+router.patch("/:id", genreController.editGenre);
+router.delete("/:id", genreController.deleteGenre);
 
+router.get("/", genreController.getAllGenres);
+router.get("/:id", genreController.getGenreById);
+router.patch("/:id/add-movie/:movieId", genreController.pushMovieId);
+router.patch("/:id/remove-movie/:movieId", genreController.pullMovieId);
 
+//to test
+router.get("/test/:id", genreController.populateMovie);
 
-module.exports= router ; 
+module.exports = router;
