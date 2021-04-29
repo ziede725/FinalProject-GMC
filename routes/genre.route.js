f23dc0422acd0752ac27ace58fbe25cffd09c226
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2021-04-29 15:22:34 +0100

    Bug fixed

--- routes/genre.route.js
+++ routes/genre.route.js
@@ -1,23 +1,21 @@
+const router = require("express").Router();
+const {
+  getGenre,
+  getAllGenres,
+  editGenre,
+  createGenre,
+  deleteGenre,
+} = require("../controllers/genreController");
 
-const router = require('express').Router() ; 
-const {getGenre,getAllGenres,editGenre,createGenre,deleteGenre} = require('../controllers/genreController') ; 
-
-
-// Find all Genres 
-router.get('/',getAllGenres) ; 
-//CreateGenre 
-router.post('/',createGenre) ;
-//Find specific genre by id ; 
-router.get('/:id', getGenre) ; 
-// Delete particular genre ; 
-router.delete('/:id',deleteGenre) ; 
-//edit specific genre ; 
-router.patch('/:id' , editGenre)
-
-router.patch("/:id/add-movie/:movieId", genreController.pushMovieId);
-router.patch("/:id/remove-movie/:movieId", genreController.pullMovieId);
-
-//to test
-router.get("/test/:id", genreController.populateMovie);
+// Find all Genres
+router.get("/", getAllGenres);
+//CreateGenre
+router.post("/", createGenre);
+//Find specific genre by id ;
+router.get("/:id", getGenre);
+// Delete particular genre ;
+router.delete("/:id", deleteGenre);
+//edit specific genre ;
+router.patch("/:id", editGenre);
 
 module.exports = router;
