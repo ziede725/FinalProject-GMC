9eaa129991d434856f091f19da879b42da6483bd
ziede <ziede.ben.yahya@gmail.com>
2021-06-08 17:42:01 +0100

    changed dotenv to production

--- Server/server.js
+++ Server/server.js
@@ -6,12 +6,12 @@ const app = express();
 require("./config/db");
 const errorHandler = require("./middlewares/error");
 //Middlewares
-var corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 //Routes
 
@@ -50,5 +50,5 @@ app.use(errorHandler);
 
 
 
-const port = process.env.PORT || 7200;
+const port = process.env.PORT || 3000 ;
 app.listen(port, () => console.log(`Server is running on ${port}`));
