188e785247342fd64b482fe4a3770eb7f2e11c8c
ziede <ziede.ben.yahya@gmail.com>
2021-06-09 07:56:07 +0100

    deploy

--- config/db.js
+++ config/db.js
@@ -8,7 +8,6 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-console.log(process.env.MONGO_URI)
 const database = mongoose
   .connect(process.env.MONGO_URI, options)
   .then(() => console.log("Connected to database."))
