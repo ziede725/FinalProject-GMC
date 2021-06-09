85e340802ec9653f80a4773591ba4fe4de26ec96
ziede <ziede.ben.yahya@gmail.com>
2021-06-09 07:58:01 +0100

    deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 if(process.env.NODE_ENV === "production")
 {
   console.log('hello im here')
-  app.use(express.static('/Client/build')) ; 
+  app.use(express.static(path.join(__dirname,'/Client/build'))) ; 
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'Client','build','index.html'))
   })
