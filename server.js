0c6f36ac64fea77953f438c275fed147137a2007
ziede <ziede.ben.yahya@gmail.com>
2021-06-11 06:36:10 +0100

    deploy fix

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 require("./config/db");
 const errorHandler = require("./middlewares/error");
 //Middlewares
-var corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 app.use(cors());
 app.use(express.json());
