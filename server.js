11d616638cd96abb06473e6da1e3c4eed7ec2f4d
ziede <ziede.ben.yahya@gmail.com>
2021-06-11 06:50:08 +0100

    testing bug on api calls

--- server.js
+++ server.js
@@ -15,15 +15,21 @@ app.use(cors());
 app.use(express.json());
 //Routes
 
-if(process.env.NODE_ENV === "production")
-{
-  console.log('hello im here')
-  // app.use(express.static(path.join(__dirname,'/Client/build'))) ; 
-  app.use(express.static('Client/build')) ; 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Client','build','index.html'))
-  })
-} 
+// if(process.env.NODE_ENV === "production")
+// {
+//   console.log('hello im here')
+//   // app.use(express.static(path.join(__dirname,'/Client/build'))) ; 
+//   app.use(express.static('Client/build')) ; 
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'Client','build','index.html'))
+//   })
+//}
+if (process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, "Client/build")));
+  app.get("/*", (_, res) => {
+   res.sendFile(path.join(__dirname, "Client/build", "index.html"));
+  });
+}
 // else {
 //   
 // }
